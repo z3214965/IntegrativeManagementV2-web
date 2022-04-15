8093942293c50c72b31fad08df54dd60744d98db
yiyi.zhang <yiyi.zhang@dilutech.com>
2022-04-15 18:49:54 +0800

    更改运行端口号

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ export default defineConfig(({ mode, command }) => {
     },
     // vite 相关配置
     server: {
-      port: 80,
+      port: 4301,
       host: true,
       open: true,
       proxy: {
