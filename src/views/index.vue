299b5bc40bd4d0d8cd4b70cc47588b18e4246960
yiyi.zhang <yiyi.zhang@dilutech.com>
2022-04-15 18:31:25 +0800

    首次提交

---
+++ src/views/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="app-container">综合管理系统</div>
+</template>
+
+<script setup name="Index">
+function goTarget(url) {
+  window.open(url, "__blank");
+}
+</script>
+
+<style scoped lang="scss"></style>
