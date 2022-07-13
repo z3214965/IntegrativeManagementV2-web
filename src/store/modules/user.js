4d9f2075a21c0623efcf611dc3140ab485cadd2e
chepuhui <c17317427394@163.com>
2022-07-13 15:38:22 +0800

    1·更改进入综合管理界面权限 2·更改存储信息位置

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -104,17 +104,17 @@ const user = {
 
     //获取从其他平台跳转来存储的参数
     getOtherPlatformsParameter() {
-      return JSON.parse(localStorage.getItem(DL_HREF) || '{}');
+      return JSON.parse(sessionStorage.getItem(DL_HREF) || '{}');
     },
 
     //设置从其他平台跳转来存储参数
     setOtherPlatformsParameter(context, data) {
-      localStorage.setItem(DL_HREF, JSON.stringify(data));
+      sessionStorage.setItem(DL_HREF, JSON.stringify(data));
     },
 
     //删除从其他平台跳转来存储的参数
     deleteOtherPlatformsParameter() {
-      localStorage.removeItem(DL_HREF);
+      sessionStorage.removeItem(DL_HREF);
     },
   },
 };
