d8f3b9fcecbe4f831f358f7f8b219173ec1cbe85
z3214965 <zhang_yiyi1987@163.com>
2022-07-15 10:26:32 +0800

    官方维护：2022-07-14

--- src/views/register.vue
+++ src/views/register.vue
@@ -17,7 +17,7 @@
           <template #prefix><svg-icon icon-class="password" class="el-input__icon input-icon" /></template>
         </el-input>
       </el-form-item>
-      <el-form-item prop="code" v-if="captchaOnOff">
+      <el-form-item prop="code" v-if="captchaEnabled">
         <el-input size="large" v-model="registerForm.code" auto-complete="off" placeholder="验证码" style="width: 63%" @keyup.enter="handleRegister">
           <template #prefix><svg-icon icon-class="validCode" class="el-input__icon input-icon" /></template>
         </el-input>
@@ -45,10 +45,8 @@
 <script setup>
 import { ElMessageBox } from 'element-plus';
 import { getCodeImg, register } from '@/api/login';
-
 const router = useRouter();
 const { proxy } = getCurrentInstance();
-
 const registerForm = ref({
   username: '',
   password: '',
@@ -56,7 +54,6 @@ const registerForm = ref({
   code: '',
   uuid: '',
 });
-
 const equalToPassword = (rule, value, callback) => {
   if (registerForm.value.password !== value) {
     callback(new Error('两次输入的密码不一致'));
@@ -64,25 +61,14 @@ const equalToPassword = (rule, value, callback) => {
     callback();
   }
 };
-
 const registerRules = {
   username: [
     { required: true, trigger: 'blur', message: '请输入您的账号' },
-    {
-      min: 2,
-      max: 20,
-      message: '用户账号长度必须介于 2 和 20 之间',
-      trigger: 'blur',
-    },
+    { min: 2, max: 20, message: '用户账号长度必须介于 2 和 20 之间', trigger: 'blur' },
   ],
   password: [
     { required: true, trigger: 'blur', message: '请输入您的密码' },
-    {
-      min: 5,
-      max: 20,
-      message: '用户密码长度必须介于 5 和 20 之间',
-      trigger: 'blur',
-    },
+    { min: 5, max: 20, message: '用户密码长度必须介于 5 和 20 之间', trigger: 'blur' },
   ],
   confirmPassword: [
     { required: true, trigger: 'blur', message: '请再次输入您的密码' },
@@ -90,11 +76,9 @@ const registerRules = {
   ],
   code: [{ required: true, trigger: 'change', message: '请输入验证码' }],
 };
-
 const codeUrl = ref('');
 const loading = ref(false);
-const captchaOnOff = ref(true);
-
+const captchaEnabled = ref(true);
 function handleRegister() {
   proxy.$refs.registerRef.validate((valid) => {
     if (valid) {
@@ -113,24 +97,22 @@ function handleRegister() {
         })
         .catch(() => {
           loading.value = false;
-          if (captchaOnOff) {
+          if (captchaEnabled) {
             getCode();
           }
         });
     }
   });
 }
-
 function getCode() {
   getCodeImg().then((res) => {
-    captchaOnOff.value = res.captchaOnOff === undefined ? true : res.captchaOnOff;
-    if (captchaOnOff.value) {
+    captchaEnabled.value = res.captchaEnabled === undefined ? true : res.captchaEnabled;
+    if (captchaEnabled.value) {
       codeUrl.value = 'data:image/gif;base64,' + res.img;
       registerForm.value.uuid = res.uuid;
     }
   });
 }
-
 getCode();
 </script>
 
@@ -148,7 +130,6 @@ getCode();
   text-align: center;
   color: #707070;
 }
-
 .register-form {
   border-radius: 6px;
   background: #ffffff;
