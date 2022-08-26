6b00ab1062aed3bea8264fd0f9a38efe7121daa8
z3214965 <zhang_yiyi1987@163.com>
2022-08-26 10:45:53 +0800

    官方维护：2022-08-24

--- src/views/system/user/index.vue
+++ src/views/system/user/index.vue
@@ -236,8 +236,7 @@
 
 <script setup name="User">
 import { getToken } from '@/utils/auth';
-import { treeselect } from '@/api/system/dept';
-import { changeUserStatus, listUser, resetUserPwd, delUser, getUser, updateUser, addUser } from '@/api/system/user';
+import { changeUserStatus, listUser, resetUserPwd, delUser, getUser, updateUser, addUser, deptTreeSelect } from '@/api/system/user';
 
 const router = useRouter();
 const { proxy } = getCurrentInstance();
@@ -297,37 +296,15 @@ const data = reactive({
   rules: {
     userName: [
       { required: true, message: '用户名称不能为空', trigger: 'blur' },
-      {
-        min: 2,
-        max: 20,
-        message: '用户名称长度必须介于 2 和 20 之间',
-        trigger: 'blur',
-      },
+      { min: 2, max: 20, message: '用户名称长度必须介于 2 和 20 之间', trigger: 'blur' },
     ],
     nickName: [{ required: true, message: '用户昵称不能为空', trigger: 'blur' }],
     password: [
       { required: true, message: '用户密码不能为空', trigger: 'blur' },
-      {
-        min: 5,
-        max: 20,
-        message: '用户密码长度必须介于 5 和 20 之间',
-        trigger: 'blur',
-      },
+      { min: 5, max: 20, message: '用户密码长度必须介于 5 和 20 之间', trigger: 'blur' },
     ],
-    email: [
-      {
-        type: 'email',
-        message: '请输入正确的邮箱地址',
-        trigger: ['blur', 'change'],
-      },
-    ],
-    phonenumber: [
-      {
-        pattern: /^1[3|4|5|6|7|8|9][0-9]\d{8}$/,
-        message: '请输入正确的手机号码',
-        trigger: 'blur',
-      },
-    ],
+    email: [{ type: 'email', message: '请输入正确的邮箱地址', trigger: ['blur', 'change'] }],
+    phonenumber: [{ pattern: /^1[3|4|5|6|7|8|9][0-9]\d{8}$/, message: '请输入正确的手机号码', trigger: 'blur' }],
   },
 });
 
@@ -343,8 +320,8 @@ watch(deptName, (val) => {
   proxy.$refs['deptTreeRef'].filter(val);
 });
 /** 查询部门下拉树结构 */
-function getTreeselect() {
-  treeselect().then((response) => {
+function getDeptTree() {
+  deptTreeSelect().then((response) => {
     deptOptions.value = response.data;
   });
 }
@@ -480,15 +457,6 @@ const handleFileSuccess = (response, file, fileList) => {
 function submitFileForm() {
   proxy.$refs['uploadRef'].submit();
 }
-/** 初始化部门数据 */
-function initTreeData() {
-  // 判断部门的数据是否存在，存在不获取，不存在则获取
-  if (deptOptions.value === undefined) {
-    treeselect().then((response) => {
-      deptOptions.value = response.data;
-    });
-  }
-}
 /** 重置操作表单 */
 function reset() {
   form.value = {
@@ -515,7 +483,6 @@ function cancel() {
 /** 新增按钮操作 */
 function handleAdd() {
   reset();
-  initTreeData();
   getUser().then((response) => {
     postOptions.value = response.posts;
     roleOptions.value = response.roles;
@@ -527,7 +494,6 @@ function handleAdd() {
 /** 修改按钮操作 */
 function handleUpdate(row) {
   reset();
-  initTreeData();
   const userId = row.userId || ids.value;
   getUser(userId).then((response) => {
     form.value = response.data;
@@ -561,6 +527,6 @@ function submitForm() {
   });
 }
 
-getTreeselect();
+getDeptTree();
 getList();
 </script>
