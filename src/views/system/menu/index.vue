e79b84e3a3485f244c9aca7e69a85a2473d20d83
z3214965 <zhang_yiyi1987@163.com>
2022-12-09 09:45:56 +0800

    官方维护：2022-12-08

--- src/views/system/menu/index.vue
+++ src/views/system/menu/index.vue
@@ -74,6 +74,7 @@
                 value-key="menuId"
                 placeholder="选择上级菜单"
                 check-strictly
+                :render-after-expand="false"
               />
             </el-form-item>
           </el-col>
