48c176dc7ec5d61161fe486144ff04451b2dc9cc
z3214965 <zhang_yiyi1987@163.com>
2023-03-08 16:00:17 +0800

    官方维护：2023-03-06

--- src/main.js
+++ src/main.js
@@ -30,6 +30,8 @@ import { parseTime, resetForm, addDateRange, handleTree, selectDictLabel, select
 import Pagination from '@/components/Pagination';
 // 自定义表格工具组件
 import RightToolbar from '@/components/RightToolbar';
+// 富文本组件
+import Editor from '@/components/Editor';
 // 文件上传组件
 import FileUpload from '@/components/FileUpload';
 // 图片上传组件
@@ -61,6 +63,7 @@ app.component('FileUpload', FileUpload);
 app.component('ImageUpload', ImageUpload);
 app.component('ImagePreview', ImagePreview);
 app.component('RightToolbar', RightToolbar);
+app.component('Editor', Editor);
 
 app.use(router);
 app.use(store);
