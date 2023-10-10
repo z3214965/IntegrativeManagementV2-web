ecd2879366bf307c991180f67fe7186f5411d7fd
z3214965 <zhang_yiyi1987@163.com>
2023-10-10 11:32:01 +0800

    官方维护：2023-10-09

--- src/components/HeaderSearch/index.vue
+++ src/components/HeaderSearch/index.vue
@@ -85,7 +85,7 @@ function initFuse(list) {
 }
 // Filter out the routes that can be displayed in the sidebar
 // And generate the internationalized title
-function generateRoutes(routes, basePath = '', prefixTitle = [], query = {}) {
+function generateRoutes(routes, basePath = '', prefixTitle = []) {
   let res = [];
 
   for (const r of routes) {
@@ -114,7 +114,7 @@ function generateRoutes(routes, basePath = '', prefixTitle = [], query = {}) {
 
     // recursive child routes
     if (r.children) {
-      const tempRoutes = generateRoutes(r.children, data.path, data.title, data.query);
+      const tempRoutes = generateRoutes(r.children, data.path, data.title);
       if (tempRoutes.length >= 1) {
         res = [...res, ...tempRoutes];
       }
