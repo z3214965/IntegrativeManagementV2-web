e5f841e93c4b3bca7101c1f4569e676fd509e3ab
z3214965 <zhang_yiyi1987@163.com>
2023-10-30 11:25:14 +0800

    官方维护：2023-10-27

--- src/components/ImagePreview/index.vue
+++ src/components/ImagePreview/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <el-image :src="`${realSrc}`" fit="cover" :style="`width:${realWidth};height:${realHeight};`" :preview-src-list="realSrcList" append-to-body="true">
+  <el-image :src="`${realSrc}`" fit="cover" :style="`width:${realWidth};height:${realHeight};`" :preview-src-list="realSrcList" :preview-teleported="true">
     <template #error>
       <div class="image-slot">
         <el-icon><picture-filled /></el-icon>
