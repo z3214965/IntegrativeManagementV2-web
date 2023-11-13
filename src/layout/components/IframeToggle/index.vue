f489cf17917668b91dbcca53cfa804694e362606
z3214965 <zhang_yiyi1987@163.com>
2023-11-13 10:18:41 +0800

    官方维护：2023-11-10

--- src/layout/components/IframeToggle/index.vue
+++ src/layout/components/IframeToggle/index.vue
@@ -5,7 +5,7 @@
       :key="item.path"
       :iframeId="'iframe' + index"
       v-show="route.path === item.path"
-      :src="item.meta.link"
+      :src="iframeUrl(item.meta.link, item.query)"
     ></inner-link>
   </transition-group>
 </template>
@@ -15,4 +15,14 @@ import InnerLink from '../InnerLink/index';
 import useTagsViewStore from '@/store/modules/tagsView';
 const route = useRoute();
 const tagsViewStore = useTagsViewStore();
+
+function iframeUrl(url, query) {
+  if (Object.keys(query).length > 0) {
+    let params = Object.keys(query)
+      .map((key) => key + '=' + query[key])
+      .join('&');
+    return url + '?' + params;
+  }
+  return url;
+}
 </script>
