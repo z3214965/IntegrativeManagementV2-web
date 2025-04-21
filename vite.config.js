f5c3b71182f98d2b37cad9947a5672d3b6dfa55f
z3214965 <zhang_yiyi1987@163.com>
2025-04-21 09:29:39 +0800

    官方维护：2025-04-18

--- vite.config.js
+++ vite.config.js
@@ -23,6 +23,21 @@ export default defineConfig(({ mode, command }) => {
       // https://cn.vitejs.dev/config/#resolve-extensions
       extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
     },
+    // 打包配置
+    build: {
+      // https://vite.dev/config/build-options.html
+      sourcemap: command === 'build' ? false : 'inline',
+      outDir: 'dist',
+      assetsDir: 'assets',
+      chunkSizeWarningLimit: 2000,
+      rollupOptions: {
+        output: {
+          chunkFileNames: 'static/js/[name]-[hash].js',
+          entryFileNames: 'static/js/[name]-[hash].js',
+          assetFileNames: 'static/[ext]/[name]-[hash].[ext]',
+        },
+      },
+    },
     // vite 相关配置
     server: {
       port: 4301,
@@ -31,8 +46,8 @@ export default defineConfig(({ mode, command }) => {
       proxy: {
         // https://cn.vitejs.dev/config/#server-proxy
         '/dev-api': {
-          target: `http://123.57.78.208:5001`,
-          // target: `http://localhost:5001`,
+          // target: `http://123.57.78.208:5001`,
+          target: `http://localhost:5001`,
           // target: `http://47.96.137.124:5001`,
           // target: `https://im.dilutech.com/prod-api`,
           changeOrigin: true,
