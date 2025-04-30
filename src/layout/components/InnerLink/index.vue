883acd1394a2f5154ed13f28f4e5d1d3f0a632dc
z3214965 <zhang_yiyi1987@163.com>
2025-04-30 09:26:27 +0800

    官方维护：2025-04-30

--- src/layout/components/InnerLink/index.vue
+++ src/layout/components/InnerLink/index.vue
@@ -1,6 +1,6 @@
 <template>
-  <div :style="'height:' + height">
-    <iframe :id="iframeId" style="width: 100%; height: 100%" :src="src" frameborder="no" title=""></iframe>
+  <div :style="'height:' + height" v-loading="loading" element-loading-text="正在加载页面，请稍候！">
+    <iframe :id="iframeId" style="width: 100%; height: 100%" :src="src" ref="iframeRef" frameborder="no" title=""></iframe>
   </div>
 </template>
 
@@ -15,5 +15,15 @@ const props = defineProps({
   },
 });
 
+const loading = ref(true);
 const height = ref(document.documentElement.clientHeight - 94.5 + 'px');
+const iframeRef = ref(null);
+
+onMounted(() => {
+  if (iframeRef.value) {
+    iframeRef.value.onload = () => {
+      loading.value = false;
+    };
+  }
+});
 </script>
