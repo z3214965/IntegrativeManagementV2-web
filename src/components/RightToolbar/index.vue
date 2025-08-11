512c6435271352cb0a452302a145623719106917
z3214965 <zhang_yiyi1987@163.com>
2025-08-11 12:10:49 +0800

    官方维护：2025-08-09

--- src/components/RightToolbar/index.vue
+++ src/components/RightToolbar/index.vue
@@ -7,7 +7,7 @@
       <el-tooltip class="item" effect="dark" content="刷新" placement="top">
         <el-button circle icon="Refresh" @click="refresh()" />
       </el-tooltip>
-      <el-tooltip class="item" effect="dark" content="显隐列" placement="top" v-if="columns">
+      <el-tooltip class="item" effect="dark" content="显隐列" placement="top" v-if="Object.keys(columns).length > 0"></el-tooltip>
         <el-button circle icon="Menu" @click="showColumn()" v-if="showColumnsType == 'transfer'" />
         <el-dropdown trigger="click" :hide-on-click="false" style="padding-left: 12px" v-if="showColumnsType == 'checkbox'">
           <el-button circle icon="Menu" />
@@ -18,9 +18,9 @@
                 <el-checkbox :indeterminate="isIndeterminate" v-model="isChecked" @change="toggleCheckAll"> 列展示 </el-checkbox>
               </el-dropdown-item>
               <div class="check-line"></div>
-              <template v-for="item in columns" :key="item.key">
+              <template v-for="(item, key) in columns" :key="item.key">
                 <el-dropdown-item>
-                  <el-checkbox v-model="item.visible" @change="checkboxChange($event, item.label)" :label="item.label" />
+                  <el-checkbox v-model="item.visible" @change="checkboxChange($event, key)" :label="item.label" />
                 </el-dropdown-item>
               </template>
             </el-dropdown-menu>
@@ -29,7 +29,7 @@
       </el-tooltip>
     </el-row>
     <el-dialog :title="title" v-model="open" append-to-body>
-      <el-transfer :titles="['显示', '隐藏']" v-model="value" :data="columns" @change="dataChange"></el-transfer>
+      <el-transfer :titles="['显示', '隐藏']" v-model="value" :data="transferData" @change="dataChange"></el-transfer>
     </el-dialog>
   </div>
 </template>
@@ -41,9 +41,10 @@ const props = defineProps({
     type: Boolean,
     default: true,
   },
-  /* 显隐列信息 */
+  /* 显隐列信息（数组格式、对象格式） */
   columns: {
-    type: Array,
+    type: [Array, Object],
+    default: () => ({})
   },
   /* 是否显示检索图标 */
   search: {
@@ -81,10 +82,19 @@ const style = computed(() => {
 
 // 是否全选/半选 状态
 const isChecked = computed({
-  get: () => props.columns.every((col) => col.visible),
+  get: () => (Array.isArray(props.columns) ? props.columns.every((col) => col.visible) : Object.values(props.columns).every((col) => col.visible)),
   set: () => {},
 });
-const isIndeterminate = computed(() => props.columns.some((col) => col.visible) && !isChecked.value);
+const isIndeterminate = computed(() =>
+  Array.isArray(props.columns)
+    ? props.columns.some((col) => col.visible) && !isChecked.value
+    : Object.values(props.columns).some((col) => col.visible) && !isChecked.value
+);
+const transferData = computed(() =>
+  Array.isArray(props.columns)
+    ? props.columns.map((item, index) => ({ key: index, label: item.label }))
+    : Object.keys(props.columns).map((key, index) => ({ key: index, label: props.columns[key].label }))
+);
 
 // 搜索
 function toggleSearch() {
@@ -98,9 +108,15 @@ function refresh() {
 
 // 右侧列表元素变化
 function dataChange(data) {
-  for (let item in props.columns) {
-    const key = props.columns[item].key;
-    props.columns[item].visible = !data.includes(key);
+  if (Array.isArray(props.columns)) {
+    for (let item in props.columns) {
+      const key = props.columns[item].key;
+      props.columns[item].visible = !data.includes(key);
+    }
+  } else {
+    Object.keys(props.columns).forEach((key, index) => {
+      props.columns[key].visible = !data.includes(index);
+    });
   }
 }
 
@@ -110,23 +126,39 @@ function showColumn() {
 }
 
 if (props.showColumnsType == 'transfer') {
-  // 显隐列初始默认隐藏列
-  for (let item in props.columns) {
-    if (props.columns[item].visible === false) {
-      value.value.push(parseInt(item));
+  // transfer穿梭显隐列初始默认隐藏列
+  if (Array.isArray(props.columns)) {
+    for (let item in props.columns) {
+      if (props.columns[item].visible === false) {
+        value.value.push(parseInt(item));
+      }
     }
+  } else {
+    Object.keys(props.columns).forEach((key, index) => {
+      if (props.columns[key].visible === false) {
+        value.value.push(index);
+      }
+    });
   }
 }
 
 // 单勾选
-function checkboxChange(event, label) {
-  props.columns.filter((item) => item.label == label)[0].visible = event;
+function checkboxChange(event, key) {
+  if (Array.isArray(props.columns)) {
+    props.columns.filter((item) => item.key == key)[0].visible = event;
+  } else {
+    props.columns[key].visible = event;
+  }
 }
 
 // 切换全选/反选
 function toggleCheckAll() {
   const newValue = !isChecked.value;
-  props.columns.forEach((col) => (col.visible = newValue));
+  if (Array.isArray(props.columns)) {
+    props.columns.forEach((col) => (col.visible = newValue));
+  } else {
+    Object.values(props.columns).forEach((col) => (col.visible = newValue));
+  }
 }
 </script>
 
